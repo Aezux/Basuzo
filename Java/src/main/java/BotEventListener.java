82944caf9839ac597682a9455afbf9db9f2c5a77
Adrian <adrianhyc@gmail.com>
2019-01-17 21:33:43 -0600

    added mute command

--- Java/src/main/java/BotEventListener.java
+++ Java/src/main/java/BotEventListener.java
@@ -2,13 +2,51 @@ package main.java;
 
 import main.java.commands.*;
 import main.java.util.Account;
+import main.java.util.Embed;
 import main.java.util.WordDetection;
+import net.dv8tion.jda.core.entities.Emote;
+import net.dv8tion.jda.core.entities.Game;
+import net.dv8tion.jda.core.entities.MessageEmbed;
+import net.dv8tion.jda.core.events.ReadyEvent;
+import net.dv8tion.jda.core.events.guild.member.GuildMemberJoinEvent;
+import net.dv8tion.jda.core.events.guild.member.GuildMemberLeaveEvent;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 public class BotEventListener extends ListenerAdapter{
+	
+	/* Bot is turned on */
+	public void onReady(ReadyEvent event) {
+		Game game = Game.playing("~help: view commands!");
+		event.getJDA().getPresence().setGame(game);
+	}
 
-	public void onMessageReceived(MessageReceivedEvent event) {
+    /* Member joins the server */
+    public void onGuildMemberJoin(GuildMemberJoinEvent event) {
+    	String user = event.getUser().getName();
+    	String guild = event.getGuild().getName();
+    	String msg = new StringBuilder("Lets all welcome **")
+    			.append(user).append("** to ").append(guild).toString();
+    	
+    	Emote emote = event.getJDA().getGuildsByName("BotIcons", false).get(0).getEmotesByName("hello", true).get(0);
+    	MessageEmbed embed = Embed.getInstance().status(msg);
+    	event.getGuild().getDefaultChannel().sendMessage(embed).complete()
+    		.addReaction(emote).complete();
+    }
+    
+    /* Member leaves the server */
+    public void onGuildMemberLeave(GuildMemberLeaveEvent event) {
+    	String user = event.getUser().getName();
+    	String msg = new StringBuilder("**")
+    			.append(user).append("** is no longer part of this server").toString();
+    	
+    	Emote emote = event.getJDA().getGuildsByName("BotIcons", false).get(0).getEmotesByName("respects", true).get(0);
+    	MessageEmbed embed = Embed.getInstance().status(msg);
+    	event.getGuild().getDefaultChannel().sendMessage(embed).complete()
+    		.addReaction(emote).complete();
+    }
+    
+    public void onMessageReceived(MessageReceivedEvent event) {
 		if (event.getAuthor().isBot()) return; // Don't reply to bots
 		if (event.getGuild() == null) return; // Don't reply to private messages
 		
@@ -29,7 +67,6 @@ public class BotEventListener extends ListenerAdapter{
 				case "poll": thread = new Thread(new Poll(event)); break;
 				case "8ball": thread = new Thread(new EightBall(event)); break;
 				case "mute": thread = new Thread(new Mute(event)); break;
-				case "lock": thread = new Thread(new Lock(event)); break;
 				default: thread = null;
 			}
 			
@@ -41,4 +78,5 @@ public class BotEventListener extends ListenerAdapter{
 		else new Thread(new WordDetection(event)).start();
 
 	}
+
 }
