1fc2d0a1da8df3119202044575edbbf1aa2b981f
Adrian <adrianhyc@gmail.com>
2019-02-03 16:57:08 -0600

    rng now uses Random.org

--- src/main/java/util/RNG.java
+++ src/main/java/util/RNG.java
@@ -1,5 +1,10 @@
 package util;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Random;
 
 public class RNG {
@@ -15,7 +20,22 @@ public class RNG {
 	}
 	
 	public int generateNumber(int maxRandom) {
-		return rand.nextInt(maxRandom);
+
+		int result = -1;
+		String request = new StringBuilder("https://www.random.org/integers/?num=1&min=0&max=")
+			.append(Integer.toString(maxRandom)).append("&col=1&base=10&format=plain&rnd=new").toString();
+
+		try {
+			URL url = new URL(request);
+			URLConnection connection = url.openConnection();
+	        InputStream inputStream = connection.getInputStream();
+	        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+	        result = Integer.parseInt(bufferedReader.readLine());
+		} catch (Exception e) {
+			return -1;
+		}
+		
+		return result;
 	}
 
 }
