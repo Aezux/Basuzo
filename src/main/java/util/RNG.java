25f2cf73f32de03c03cbf363d9556a0a9b62b153
Adrian <adrianhyc@gmail.com>
2019-02-05 02:35:48 -0600

    took out Random.org

--- src/main/java/util/RNG.java
+++ src/main/java/util/RNG.java
@@ -1,16 +1,12 @@
 package util;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
+import java.security.SecureRandom;
 import java.util.Random;
 
 public class RNG {
 	
 	public static RNG instance = null;
-	Random rand = new Random();
+	Random rand = new SecureRandom();
 	
 	private RNG() {}
 	
@@ -20,22 +16,7 @@ public class RNG {
 	}
 	
 	public int generateNumber(int maxRandom) {
-
-		int result = 0;
-		String request = new StringBuilder("https://www.random.org/integers/?num=1&min=0&max=")
-			.append(Integer.toString(maxRandom)).append("&col=1&base=10&format=plain&rnd=new").toString();
-
-		try {
-			URL url = new URL(request);
-			URLConnection connection = url.openConnection();
-	        InputStream inputStream = connection.getInputStream();
-	        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-	        result = Integer.parseInt(bufferedReader.readLine());
-		} catch (Exception e) {
-			return rand.nextInt(maxRandom);
-		}
-		
-		return result;
+		return rand.nextInt(maxRandom);
 	}
 
 }
